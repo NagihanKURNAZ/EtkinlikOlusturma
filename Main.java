42e8c0f893a4cde574904e3e8f3694e27769aca3
NagihanKURNAZ <112775522+NagihanKURNAZ@users.noreply.github.com>
2022-12-31 11:55:31 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		int heat;
+		
+		 Scanner input = new Scanner(System.in);
+		 
+		 System.out.println("Sıcaklık giriniz");
+		 heat = input.nextInt();
+		 
+		 if(heat<5) {
+			 System.out.println("Kayak yapabilirsin.");
+		 }
+		 else if(heat>=5 && heat<=25){
+			
+			 if(heat<=15) {
+				 System.out.println("Sinemaya gidebilirsin");
+			 } 
+			 if(heat>=10) {
+				 System.out.println("Pikniğe gidebilirsin");
+			 }
+		 }
+		 else{
+			 
+			 System.out.println("Yüzmeye gidebilirsin.");
+		 }
+		 
+
+	}
+
+}
